c376d112760c79063baf530dfd64081951235957
R0zetka <denis.kr2001@gmail.com>
2019-07-26 18:33:37 +0300

    V-1

---
+++ border.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class border : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("SnakeMain"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+    }
+}
---
+++ point.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class point : MonoBehaviour {
+
+    public GameObject foodPrefab;
+    public Vector3 curPos;
+    public GameObject curFood;
+
+	
+    void AddNewFood(){
+        RandomPos();
+        curFood = GameObject.Instantiate(foodPrefab, curPos, Quaternion.identity) as GameObject;
+    
+    }
+
+    void RandomPos(){
+        curPos = new Vector3(Random.Range(-9, 9), 1f, Random.Range(-7, 7));
+    }
+    void Update()
+    {
+        if(!curFood){
+            AddNewFood();
+        }
+        else{
+            return;
+        }
+    }
+}
