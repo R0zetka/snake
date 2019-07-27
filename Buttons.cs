d54cac23627cbbb5fc8e6e8afdaf98869332923f
R0zetka <50676756+R0zetka@users.noreply.github.com>
2019-07-27 03:10:20 +0300

    Add files via upload

---
+++ Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour {
+
+  
+
+    void OnMouseUpAsButton()
+    {
+        switch(gameObject.name){
+            case"exit":
+                Application.Quit();
+                break;
+            case"play":
+                Application.LoadLevel("Snake");
+                break;
+        }
+    }
+}
---
+++ CubeRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour {
+
+    public float Speed;
+
+
+	void OnMouseOver () {
+        transform.Rotate(Vector3.right * Time.deltaTime * Speed);
+
+	}
+}
